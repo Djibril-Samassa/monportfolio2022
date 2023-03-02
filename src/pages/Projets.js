4a421597b2b47c77fec6fe57385c8706b9ec158c
Djibril <djibrilsamassa.pro@gmail.com>
2023-03-02 13:53:46 +0100

    e-books

--- src/pages/Projets.js
+++ src/pages/Projets.js
@@ -75,6 +75,10 @@ export default function Projets() {
             <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
             <a href="https://meteo-by-djibril.netlify.app/" target="_blank">Météo</a>
           </li>
+          <li className="unselectedSkill link">
+            <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
+            <a href="https://e-books-by-djibril.netlify.app/" target="_blank">E-books</a>
+          </li>
         </ul>
       </Slide>
       <Slide top>
