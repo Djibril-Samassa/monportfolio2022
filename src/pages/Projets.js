19b28d2a0f3669965d5ce6e6600775ca064d6df6
Djibril <djibrilsamassa.pro@gmail.com>
2023-03-07 22:40:22 +0100

    extensions

--- src/pages/Projets.js
+++ src/pages/Projets.js
@@ -13,89 +13,54 @@ import { Icon } from "@iconify/react";
 export default function Projets() {
   const menuState = useContext(MenuContext);
   menuState.setSelected("projets");
-  const [selected, setSelected] = useState("pays");
   return (
     <div style={{ width: "90%", margin: "0 auto" }}>
       <h1 className="expTitle">Mes Projets</h1>
-      <h3 className="expTitle">
-        Un aperçu ? Choisissez ce que vous voulez tester juste en dessous{" "}
-      </h3>
       <Slide left>
-        <ul className="skillcontainer">
-          {selected === "pays" ? (
-            <li className="selectedSkill">Pays</li>
-          ) : (
-            <li
-              className="unselectedSkill"
-              onClick={() => {
-                setSelected("pays");
-              }}
-            >
-              Pays
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <h3 className="pn">Projets en ligne</h3>
+          <ul className="skillcontainer">
+            <li className="unselectedSkill link">
+              <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
+              <a href="https://distributeur-by-djibril.netlify.app/" target="_blank">Distributeur</a>
             </li>
-          )}
-          {selected === "animation" ? (
-            <li className="selectedSkill">Animation</li>
-          ) : (
-            <li
-              className="unselectedSkill"
-              onClick={() => {
-                setSelected("animation");
-              }}
-            >
-              Animation
-            </li>
-          )}
-          {selected === "colorandform" ? (
-            <li className="selectedSkill">Couleur & forme</li>
-          ) : (
-            <li
-              className="unselectedSkill"
-              onClick={() => {
-                setSelected("colorandform");
-              }}
-            >
-              Couleur & forme
-            </li>
-          )}
-          <li className="unselectedSkill link">
-            <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
-            <a href="https://distributeur-by-djibril.netlify.app/" target="_blank">Distributeur</a>
-          </li>
 
-          <li className="unselectedSkill link">
-            <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
-            <a href="https://quiz-by-djibril.netlify.app/" target="_blank">Quiz</a>
-          </li>
-          <li className="unselectedSkill link">
-            <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
-            <a href="https://ascenseur-by-djibril.netlify.app/" target="_blank">Ascenseur</a>
-          </li>
-          <li className="unselectedSkill link">
-            <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
-            <a href="https://meteo-by-djibril.netlify.app/" target="_blank">Météo</a>
-          </li>
-          <li className="unselectedSkill link">
-            <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
-            <a href="https://e-books-by-djibril.netlify.app/" target="_blank">E-books</a>
-          </li>
-          <li className="unselectedSkill link">
-            <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
-            <a href="https://my-auth-by-djibril.netlify.app/" target="_blank">myOAuth</a>
-          </li>
-        </ul>
-      </Slide>
-      <Slide top>
-        <div className="skillShowContainer">
-          {selected === "pays" ? (
-            <Pays />
-          ) : selected === "colorandform" ? (
-            <ColorAndForm />
-          ) : selected === "animation" ? (
-            <Animation />
-          ) : null}
+            <li className="unselectedSkill link">
+              <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
+              <a href="https://quiz-by-djibril.netlify.app/" target="_blank">Quiz</a>
+            </li>
+            <li className="unselectedSkill link">
+              <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
+              <a href="https://ascenseur-by-djibril.netlify.app/" target="_blank">Ascenseur</a>
+            </li>
+            <li className="unselectedSkill link">
+              <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
+              <a href="https://meteo-by-djibril.netlify.app/" target="_blank">Météo</a>
+            </li>
+            <li className="unselectedSkill link">
+              <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
+              <a href="https://e-books-by-djibril.netlify.app/" target="_blank">E-books</a>
+            </li>
+            <li className="unselectedSkill link">
+              <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
+              <a href="https://my-auth-by-djibril.netlify.app/" target="_blank">myOAuth</a>
+            </li>
+          </ul>
         </div>
-      </Slide>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <h3 className="pn">Extensions</h3>
+          <ul className="skillcontainer">
+            <li className="unselectedSkill link">
+              <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
+              <a href="https://easylink-by-djibril.netlify.app/" target="_blank">EasyLink</a>
+            </li>
+            <li className="unselectedSkill link">
+              <Icon style={{ marginRight: '5px' }} className='iconif' icon="material-symbols:open-in-new" />
+              <a href="https://error-gpt-by-djibril.netlify.app/" target="_blank">Error Gpt</a>
+            </li>
+          </ul>
+        </div>
+      </Slide >
 
       <h3 className="expTitle">Mes projets réalisés</h3>
       <Slide right>
@@ -105,6 +70,6 @@ export default function Projets() {
           })}
         </div>
       </Slide>
-    </div>
+    </div >
   );
 }
